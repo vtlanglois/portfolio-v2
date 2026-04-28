bdadc445c464711b0582fabdc927ff57519db6a1
Vincent Langlois <vtlanglo@gmail.com>
2026-04-28 19:53:55 -0400

    Add theme selectable via UI on page Fixes #19

---
+++ src/components/ui/Orb/index.tsx
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export default function Orb({
+  tag,
+  children,
+  className,
+  ...rest
+}: {
+  tag: React.ElementType;
+  children: React.ReactNode;
+  className?: string;
+  [key: string]: any;
+}) {
+  const Tag = tag || "div";
+  return (
+    <Tag
+      className={`orb rounded-full opacity-80 hover:scale-125 p-2 transition duration-300 motion-reduce:transition-none motion-reduce:transform-none ${className}`}
+      {...rest}
+    >
+      {children}
+    </Tag>
+  );
+}
---
+++ src/components/ui/ThemeSelector.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { THEMES } from "@/constants";
+import { useEffect, useState } from "react";
+import Orb from "./Orb";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import { CactusIcon, PlantIcon, WavesIcon } from "@phosphor-icons/react";
+
+export default function ThemeSelector() {
+  const [theme, setTheme] = useState(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const initialTheme = urlParams.get("theme");
+    return THEMES.includes(initialTheme || "") ? initialTheme! : "bliss";
+  });
+
+  const toggleTheme = () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const currentThemeIndex = THEMES.indexOf(currentTheme || "bliss");
+    const newTheme = THEMES[(currentThemeIndex + 1) % THEMES.length];
+    document.documentElement.setAttribute("data-theme", newTheme);
+    window.history.replaceState(null, "", `?theme=${newTheme}`);
+    setTheme(newTheme);
+  };
+
+  const variants: Variants = {
+    initial: (d: number) => ({ opacity: 0, x: d * 20, scale: 0.25,filter: "blur(4px)" }),
+    animate: { opacity: 1, x: 0, scale: 1, filter: "blur(0px)" },
+    exit: (d: number) => ({ opacity: 0, x: d * -20, scale: 0.25, filter: "blur(4px)" }),
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  return (
+    <Orb
+      tag="button"
+      onClick={toggleTheme}
+      className="bg-(--hill-near) hover:bg-(--hill-far)"
+      title="Cycle themes: bliss, ocean, dunes"
+    >
+      <AnimatePresence mode="popLayout" initial={false} custom={1}>
+        <motion.div
+          key={theme}
+          custom={1}
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{ type: "spring", duration: 0.35, bounce: 0 }}
+        >
+          <ThemeIcon theme={theme} />
+        </motion.div>
+      </AnimatePresence>
+    </Orb>
+  );
+}
+
+
+function ThemeIcon({ theme }: { theme: string }) {
+  switch (theme) {
+    case "bliss":
+      return <PlantIcon size={32} weight="duotone" />;
+    case "ocean":
+      return <WavesIcon size={32} weight="duotone" />;
+    case "dunes":
+      return <CactusIcon size={32} weight="duotone" />;
+    default:
+      return null;
+  }
