74d0e2e40054f78bca02fb1b4ad9cab422515acd
Vincent Langlois <vtlanglo@gmail.com>
2026-04-29 19:13:40 -0400

    Add experience section Fixes #7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import About from "@/components/sections/About";
 import Hero from "../components/sections/Hero";
 import Projects from "@/components/sections/Projects";
+import Experience from "@/components/sections/Experience";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Experience />
       <Projects />
     </>
   );
---
+++ src/components/sections/Experience.tsx
@@ -0,0 +1,66 @@
+import Card from "../ui/Card";
+import Container from "../ui/Container";
+import GitHubLink from "../ui/GitHubLink";
+import Section from "../ui/Section";
+import Stack from "../ui/Stack";
+
+export default function Experience() {
+  return (
+    <Section>
+      <Container>
+        <Stack>
+          <Card>
+            <h2 className="text-3xl font-bold">Experience</h2>
+          </Card>
+          <div className="grid grid-cols-1 gap-4">
+            <Card className="flex flex-col gap-2">
+              <div className="flex flex-row justify-between gap-1 flex-wrap">
+                <h3 className="text-xl font-semibold">
+                  Senior Software Engineer at Eli Lilly & Company
+                </h3>
+                <p>July 2023 - Present</p>
+              </div>
+              <p className="text-sm">Indianapolis, IN</p>
+              <p>
+                Currently working on developing scalable web applications and
+                systems to enhance the customer experience for Lilly's online
+                services, focusing on accessible user interfaces. Leading a
+                full-stack development team to deliver high-quality software
+                solutions.
+              </p>
+            </Card>
+            <Card className="flex flex-col gap-2">
+              <div className="flex flex-row justify-between gap-1 flex-wrap">
+                <h3 className="text-xl font-semibold">
+                  Software Engineer Intern at BCForward
+                </h3>
+                <p>May 2022 - August 2022</p>
+              </div>
+              <p className="text-sm">Carmel, IN</p>
+              <p>
+                Worked on developing and maintaining the Indiana Prosecutor Case
+                Management System, focused on enhancing user experience and
+                performance.
+              </p>
+            </Card>
+            <Card className="flex flex-col gap-2">
+              <div className="flex flex-row justify-between gap-1 flex-wrap">
+                <h3 className="text-xl font-semibold mb-2">
+                  Community Outreach Intern at IU Luddy School of Informatics,
+                  Computing, and Engineering
+                </h3>
+                <p>August 2020 - July 2023</p>
+              </div>
+              <p className="text-sm">Bloomington, IN</p>
+              <p>
+                Directed and coordinated community outreach programs, focused on
+                promoting STEM education and engaging with local schools to
+                inspire the next generation of technologists.
+              </p>
+            </Card>
+          </div>
+        </Stack>
+      </Container>
+    </Section>
+  );
+}
