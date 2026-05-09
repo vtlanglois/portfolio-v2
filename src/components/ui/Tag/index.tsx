7bd699300dd7b442851f566ec34f7423a8180f2b
Vincent Langlois <vtlanglo@gmail.com>
2026-05-09 13:32:32 -0400

    fix: comment out temp-unused code

--- src/components/ui/Tag/index.tsx
+++ src/components/ui/Tag/index.tsx
@@ -1,78 +1,77 @@
 import { TagItem } from "@/types/tagTypes";
-import { Icon } from "@phosphor-icons/react";
-import {
-  PersonArmsSpreadIcon,
-  RobotIcon,
-  AtomIcon,
-  PaletteIcon,
-  FigmaLogoIcon,
-  GitMergeIcon,
-  GithubLogoIcon,
-  FileCodeIcon,
-  BrowserIcon,
-  CircuitryIcon,
-  UsersIcon,
-  DatabaseIcon,
-  GameControllerIcon,
-  BugIcon,
-  LegoIcon,
-  PottedPlantIcon,
-  PersonSimpleHikeIcon,
-  CookingPotIcon,
-  MusicNoteIcon,
-  BookIcon,
-  FlaskIcon,
-  PersonIcon,
-  ScrollIcon,
-  WrenchIcon,
-  StarIcon,
-} from "@phosphor-icons/react/dist/ssr";
+// import { Icon } from "@phosphor-icons/react";
+// import {
+//   PersonArmsSpreadIcon,
+//   RobotIcon,
+//   AtomIcon,
+//   PaletteIcon,
+//   FigmaLogoIcon,
+//   GitMergeIcon,
+//   GithubLogoIcon,
+//   BrowserIcon,
+//   CircuitryIcon,
+//   UsersIcon,
+//   DatabaseIcon,
+//   GameControllerIcon,
+//   BugIcon,
+//   LegoIcon,
+//   PottedPlantIcon,
+//   PersonSimpleHikeIcon,
+//   CookingPotIcon,
+//   MusicNoteIcon,
+//   BookIcon,
+//   FlaskIcon,
+//   PersonIcon,
+//   ScrollIcon,
+//   WrenchIcon,
+//   StarIcon,
+// } from "@phosphor-icons/react/dist/ssr";
 
-const variantIcons = {
-  tech: FlaskIcon,
-  human: PersonIcon,
-  topic: ScrollIcon,
-  tool: WrenchIcon,
-  hobby: StarIcon,
-};
+// const variantIcons = {
+//   tech: FlaskIcon,
+//   human: PersonIcon,
+//   topic: ScrollIcon,
+//   tool: WrenchIcon,
+//   hobby: StarIcon,
+// };
 
 
-const icons = {
-  a11y: PersonArmsSpreadIcon,
-  robot: RobotIcon,
-  atom: AtomIcon,
-  palette: PaletteIcon,
-  figma: FigmaLogoIcon,
-  git: GitMergeIcon,
-  github: GithubLogoIcon,
-  "file-code": BrowserIcon,
-  circuit: CircuitryIcon,
-  users: UsersIcon,
-  db: DatabaseIcon,
-  controller: GameControllerIcon,
-  bug: BugIcon,
-  lego: LegoIcon,
-  "potted-plant": PottedPlantIcon,
-  hiking: PersonSimpleHikeIcon,
-  cooking: CookingPotIcon,
-  song: MusicNoteIcon,
-  book: BookIcon,
-};
+// const icons = {
+//   a11y: PersonArmsSpreadIcon,
+//   robot: RobotIcon,
+//   atom: AtomIcon,
+//   palette: PaletteIcon,
+//   figma: FigmaLogoIcon,
+//   git: GitMergeIcon,
+//   github: GithubLogoIcon,
+//   "file-code": BrowserIcon,
+//   circuit: CircuitryIcon,
+//   users: UsersIcon,
+//   db: DatabaseIcon,
+//   controller: GameControllerIcon,
+//   bug: BugIcon,
+//   lego: LegoIcon,
+//   "potted-plant": PottedPlantIcon,
+//   hiking: PersonSimpleHikeIcon,
+//   cooking: CookingPotIcon,
+//   song: MusicNoteIcon,
+//   book: BookIcon,
+// };
 
 export default function Tag({
   tag,
 }: {
   tag: TagItem;
 }) {
-  const selectedIcon = () => {
-    let IconComponent: Icon;
-    if (tag.icon && tag.icon in icons) {
-      IconComponent = icons[tag.icon as keyof typeof icons];
-      return <IconComponent weight="duotone" className="inline-block" />
-    }
-      IconComponent = variantIcons[tag.variant ?? "tool"];
-   return <IconComponent weight="duotone" className="inline-block" />
-  };
+  // const selectedIcon = () => {
+  //   let IconComponent: Icon;
+  //   if (tag.icon && tag.icon in icons) {
+  //     IconComponent = icons[tag.icon as keyof typeof icons];
+  //     return <IconComponent weight="duotone" className="inline-block" />
+  //   }
+  //     IconComponent = variantIcons[tag.variant ?? "tool"];
+  //  return <IconComponent weight="duotone" className="inline-block" />
+  // };
 
   return (
     <span
