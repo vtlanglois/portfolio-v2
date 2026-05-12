8466aa022b5e0d800a07962d180096de6d1e7a7f
Vincent Langlois <vtlanglo@gmail.com>
2026-05-11 22:16:26 -0400

    prevent skipnav from leaking thru design

--- src/components/ui/Skipnav.tsx
+++ src/components/ui/Skipnav.tsx
@@ -5,7 +5,7 @@ export default function Skipnav() {
     <Card
       size="small"
       variation="heading"
-      className=" absolute top-0 left-0 z-50 w-full -translate-y-full p-3 !rounded-t-none text-center font-bold underline focus-within:translate-y-0 motion-safe:transition-transform motion-safe:duration-200"
+      className=" absolute top-0 left-0 z-50 w-full -translate-y-[200px] p-3 !rounded-t-none text-center font-bold underline focus-within:translate-y-0 motion-safe:transition-transform motion-safe:duration-200"
     >
       <a href="#main">Skip to Main Content</a>
     </Card>
